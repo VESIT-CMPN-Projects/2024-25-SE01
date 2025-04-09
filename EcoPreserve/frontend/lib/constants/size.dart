eb672cb997565971906a888f8ab8ff947ab44e67
pankaj0695 <pankajgupta0695@gmail.com>
2025-04-09 13:42:01 +0530

    updated

--- EcoPreserve/frontend/lib/constants/size.dart
+++ EcoPreserve/frontend/lib/constants/size.dart
@@ -1,3 +1,1 @@
 const double minDesktopWidth = 600.0;
-// const apiKey='sk-proj-WIOEiVoYEM9bEaDMRRhpT3BlbkFJlqkcRdVaVN8KfVYCNoOlsan';
-// const apiKey2='sk-proj-WIOEiVoYEM9bEaDMRRhpT3BlbkFJlqkcRdVaVN8KfVYCNoOlnas';
